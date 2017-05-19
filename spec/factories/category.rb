31b1cdce7eb5e1017e61d2bc0eaa3d3c23c0b9c7
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-19 11:43:51 -0600

    categories display all related jobs

--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -1,19 +1,19 @@
 FactoryGirl.define do
+  sequence :category_title do |n|
+    "Category #{n}"
+  end
+
   factory :category do
     title {generate(:category_title)}
     factory :category_with_job do
 
-      trasient do
+      transient do
         jobs_count 1
       end
       
       after(:create) do |category, evaluator|
         create_list(:job, evaluator.jobs_count, category: category)
-  end
-
-  sequence :category_title do |n|
-    "Category #{n}"
-  end
-  
-
+      end
+    end
+  end  
 end
--- spec/features/categories/user_sees_all_jobs_category_spec.rb
+++ spec/features/categories/user_sees_all_jobs_category_spec.rb
@@ -1,4 +1,14 @@
 require "rails_helper"
 
-describe "From a category show page"
-  scenario "user can see all jobs in that category"
+describe "From a category show page" do
+  scenario "user can see all jobs in that category" do
+    category = create(:category_with_job, jobs_count: 2)
+    job1 = category.jobs.first
+    job2 = category.jobs.last
+
+    visit category_path(category)
+    save_and_open_page
+    expect(page).to have_content(job1.title)
+    expect(page).to have_content(job2.title)
+  end
+end
