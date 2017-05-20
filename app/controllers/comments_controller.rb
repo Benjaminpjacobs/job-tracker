1f459ead7d95b5406b17eb8d257f0ab85e771028
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-20 11:08:26 -0600

    create comments with correct routing and css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,20 @@ class CommentsController < ApplicationController
     @company = Company.find(params[:company_id])
     @job = Job.find(params[:job_id])
   end
+
+  def create
+    @company = Company.find(params[:company_id])
+    @job = Job.find(params[:job_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to company_job_path(@company, @job)
+    end
+    
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content, :job_id)
+    end
 end
--- spec/features/comments/user_can_create_a_comment_spec.rb
+++ spec/features/comments/user_can_create_a_comment_spec.rb
@@ -7,9 +7,9 @@ describe "When a user visits a job" do
 
     visit company_job_path(company, job)
     click_link "New Comment"
+    
     fill_in "comment[content]", with: "initial comment"
-    click_button "Create"
-
+    click_button "Create Comment"
     expect(current_path).to eq(company_job_path(company, job))
     expect(page).to have_content("initial comment")
   end
