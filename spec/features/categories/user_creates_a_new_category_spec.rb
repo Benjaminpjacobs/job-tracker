0d85f780554ae24665b0cee2049a83911f22c46c
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-20 10:01:05 -0600

    test for adding categories from every link

--- spec/features/categories/user_creates_a_new_category_spec.rb
+++ spec/features/categories/user_creates_a_new_category_spec.rb
@@ -13,6 +13,34 @@ describe "User can create a new category" do
     expect(page).to have_content(category.title)
     expect(Category.count).to eq(1)
   end
+
+  scenario "from the new category link" do
+    category = build(:category)
+    visit categories_path
+
+    click_link "Add New Category"
+
+    fill_in "category[title]", with: category.title
+    click_button "Create"
+
+    expect(current_path).to eq(category_path(Category.last))
+    expect(page).to have_content(category.title)
+    expect(Category.count).to eq(1)
+  end
+
+  scenario "from the new job link" do
+    company = create(:company)
+    category = build(:category)
+
+    visit company_path(company)
+    click_link "Add Job"
+    click_link "Create New Category"
+    fill_in "category[title]", with: category.title
+    click_button "Create"
+
+    expect(current_path).to eq(new_company_job_path(company))
+    find(:select).find(:option, "#{category.title}").click
+  end
 end
 
 describe "User cannot create a new category" do
@@ -26,5 +54,6 @@ describe "User cannot create a new category" do
     expect(page).to have_content("Title has already been taken")
   end
 end
+
 
 
