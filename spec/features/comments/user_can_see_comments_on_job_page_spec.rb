20084beea25c1d10228ceb4b998052bdb65fce94
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-05-20 18:22:40 -0600

    testingall required comments functionality

--- spec/features/comments/user_can_see_comments_on_job_page_spec.rb
+++ spec/features/comments/user_can_see_comments_on_job_page_spec.rb
@@ -2,28 +2,32 @@ require 'rails_helper'
 
 describe "When a user visits a job" do
   scenario "they can see a comment they've created" do
-    comment = create(:comment) 
-    byebug
-    # company = job.company
-    # expect(Comment.count).to eq(0)
-    # visit company_job_path(company, job)
-    # click_link "New Comment"
+    comment = build(:comment) 
+    job = create(:job)
+    company = job.company
+    visit company_job_path(company, job)
+    click_link "New Comment"
     
-    # fill_in "comment[content]", with: "initial comment"
-    # click_button "Create Comment"
+    fill_in "comment[content]", with: comment.content
+    click_button "Create Comment"
 
-    # expect(page).to have_content("initial comment")
+    expect(page).to have_content(comment.content)
   end
 
-  # scenario "they cannot create a blank comment" do
-  #   job = create(:job) 
-  #   company = job.company
-  #   visit company_job_path(company, job)
-  #   click_link "New Comment"
-  #   click_button "Create Comment"
-
-  #   expect(page).to have_content("Content can't be blank")
-  # end
+  scenario "they see the most recent comment first" do
+      comment = create(:comment)
+      job = comment.job
+      company = job.company
+      comment2 = build(:comment) 
+      visit company_job_path(company, job)
+      click_link "New Comment"
+      
+      fill_in "comment[content]", with: comment2.content
+      click_button "Create Comment"
+      
+      expect(first('.comment')).to have_content(comment2.content)
+      expect(page.all(".comment").count).to eq(2)
+  end
   
 end
 
